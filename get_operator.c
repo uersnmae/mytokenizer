a65ada9098fd52596c866296dc62d4578279a3d0
Dong Hyun Kim <dong-hki@c3r15s6.42gyeongsan.kr>
2025-04-21 03:16:54 +0900

    .

--- get_operator.c
+++ get_operator.c
@@ -6,11 +6,27 @@
 /*   By: dong-hki <dong-hki@student.42gyeongsan.kr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 03:06:45 by dong-hki          #+#    #+#             */
-/*   Updated: 2025/04/21 03:07:30 by dong-hki         ###   ########.fr       */
+/*   Updated: 2025/04/21 03:13:51 by dong-hki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tokenizer.h"
+
+t_toktype	get_operator_len_two(const char **str);
+t_toktype	get_operator_len_one(const char **str);
+
+t_toktype	get_operator(const char **str)
+{
+	t_toktype	toktype;
+
+	toktype = get_operator_len_two(str);
+	if (toktype != TK_EOF)
+		return (toktype);
+	toktype = get_operator_len_one(str);
+	if (toktype != TK_EOF)
+		return (toktype);
+	return (toktype);
+}
 
 t_toktype	get_operator_len_two(const char **str)
 {
